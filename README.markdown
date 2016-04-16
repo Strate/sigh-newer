607b551b8c9e832e25aee014f296be9a6b258cea
Artur Eshenbrener <strate@yandex.ru>
2016-04-16 17:46:16 +0300

    init

---
+++ README.markdown
@@ -0,0 +1,12 @@
+# sigh-newer
+
+[![build status](https://circleci.com/gh/Strate/sigh-newer.png)](https://circleci.com/gh/Strate/sigh-newer)
+
+Sigh plugin for...
+
+## Example
+
+`npm install --save-dev sigh-newer` then add something like this to your `sigh.js`:
+```javascript
+// TODO: example goes here
+```
---
+++ src/test/index.spec.js
@@ -0,0 +1,15 @@
+import _ from 'lodash'
+import Promise from 'bluebird'
+import { Bacon } from 'sigh-core'
+import Event from 'sigh/lib/Event'
+
+import newer from '../'
+
+require('source-map-support').install()
+require('chai').should()
+
+describe('sigh-newer', () => {
+  xit('TODO: should do something', () => {
+    // TODO:
+  })
+})
