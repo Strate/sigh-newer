7b5c374b588f9846c53e57ce544bb34487a738a1
Artur Eshenbrener <strate@yandex.ru>
2016-04-16 18:39:15 +0300

    POC

--- README.markdown
+++ README.markdown
@@ -2,11 +2,28 @@
 
 [![build status](https://circleci.com/gh/Strate/sigh-newer.png)](https://circleci.com/gh/Strate/sigh-newer)
 
-Sigh plugin for...
+Sigh plugin for filtering files, keeping only newer in stream
 
 ## Example
 
 `npm install --save-dev sigh-newer` then add something like this to your `sigh.js`:
 ```javascript
-// TODO: example goes here
+var newer, glob, write, postcss, stylus
+module.exports = function(pipelines) {
+    pipelines['assets'] = [
+      glob("src/**/*.jpg"),
+      newer("target"),
+      write("target") // writes only newer files that in target
+    ];
+    
+    pipelines['styl'] = [
+      glob("src/**/*.styl"),
+      // it can test against changed extension,
+      // in this example files filtered by searching corresponding css file in target directory
+      newer("target", "css"),
+      stylus(),
+      postcss(),
+      write("target")
+    ];
+}
 ```
--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import _ from 'lodash'
 import Promise from 'bluebird'
-import { Bacon } from 'sigh-core'
+import { Bacon, log } from 'sigh-core'
+import fs from 'fs'
+import path from 'path'
 
-export default function(op, opts = {}) {
-  // TODO: plugin code goes here
+const statAsync = Promise.promisify(fs.stat)
+
+export default function(op, basePath, targetType) {
+  return op.stream.flatMap(_originalEvents => Bacon.fromPromise(
+    Promise.filter(
+      _originalEvents,
+      event => {
+        if (event.type !== "add" && event.type !== "change") {
+          return true;
+        }
+        let targetFileName = path.join(
+          basePath,
+          path.dirname(event.path),
+          targetType
+            ? path.basename(event.path, '.'+event.fileType) + '.' + targetType
+            : path.basename(event.path)
+        )
+        return Promise.all([
+          statAsync(event.sourcePath),
+          statAsync(targetFileName)
+        ]).then(([sourceStat, targetStat]) =>
+          targetStat.mtime.getTime() < sourceStat.mtime.getTime()
+        ).catch(e => true)
+      }
+    ).then(events => {
+      log(events.length, "files to process in found", _originalEvents.length)
+      return events
+    })
+  ))
 }
